41ca48d8b602cc2aa7c1ff82e1c6dd408f86f235
Tran Cong Duc <trancongduc0812@gmail.com>
2025-06-29 21:58:06 +0700

    feat:fe-ui

--- Payment-FE/payment-sgr/src/App.jsx
+++ Payment-FE/payment-sgr/src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PaymentPage from "./pages/payment.page";
+import VNPAYReturnPage from "./pages/vnpayreturn.page";
+
 function App() {
-  return <>Xin chào</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaymentPage />} />
+        <Route path="/vnpay-return" element={<VNPAYReturnPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
